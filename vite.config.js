4d5f783fca5740330fa3215031c2d3de4eca3b20
Athar9909 <atharkatheri@gmail.com>
2025-10-07 16:22:57 +0530

    verce

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   server: {
+  server: {
     port: 5176,
   },
+  base: "/",
 });
-
